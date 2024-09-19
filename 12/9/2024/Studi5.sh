144712ed880eae3760a3a2a05ffa1a7827a4354f
cascen <djoehannesleonfamilyar@gmail.com>
2024-09-19 10:40:52 +0700

    a

---
+++ 12/9/2024/Studi5.sh
@@ -0,0 +1,44 @@
+function ngobrol_y() {
+echo "pengusaha : aku mau beli peralatan kantor"
+
+echo "enstein : baiklah,  kan pasti butuh komputer. kuantitas komputer butuh brp?"
+
+read komputer
+
+echo "enstein : harga perunit komputer brp?"
+
+read unit
+
+echo "enstein : harga meja per set?"
+
+read meja
+
+echo "enstein : harga kursi per set?"
+
+read kursi
+
+echo "enstein : modal nya brp?"
+
+read modal
+
+echo "jadi total komputer yg dibutuhin $komputer unit dan harga nya $unit berarti total nya $totalkomputer"
+
+echo "dan meja kursi masing masing harga 1 set nya $meja dan $kursi berarti total dari $komputer meja kursi itu $totalmersi"
+
+echo "kalo total semuanya bisa $totalharga dengan modal kamu yang $modal jadi ya $duitsisa"
+
+totalkomputer=$((komputer * unit))
+totalmersi=$(((meja + kursi) * komputer))
+totalharga=$((totalkomputer + totalmersi))
+duitsisa=$((modal - totalharga))
+
+if [ $duitsisa -lt 0 ]; then
+    echo "proyek melebihi budget yaitu ${duitsisa#-}"
+else
+    echo "duit aman gas aja"
+fi
+
+}
+
+
+ngobrol_y
